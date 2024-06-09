e8a47487ab12f926a86612ac33454b2e9c6d9ba1
ratamoneda <ratamoneda81@gmail.com>
2024-06-09 13:45:28 +0100

    Update start_1_4_19.sh

--- start_1_4_19.sh
+++ start_1_4_19.sh
@@ -1,5 +1,5 @@
 cd /home/quilibrium/ceremonyclient/
-git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient
+git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git || git remote set-url origin https://git.quilibrium-mirror.ch/agostbiro/ceremonyclient.git
 git fetch origin && git checkout -b release-cdn --track origin/release-cdn
 cd node
 ./release_autorun.sh
